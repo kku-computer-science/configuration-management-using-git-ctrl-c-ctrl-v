bb6197b0c4c035e83747c63412c86cfc8d4e2796
Tsuki2548 <weerawong.h@kkumail.com>
2025-12-06 17:22:21 +0700

    Add Quicksort to project

--- Project/Sorting/src/App.java
+++ Project/Sorting/src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+
+
+        //ตัวอย่างการใช้งาน QuickSort นะจ๊ะ
+        int[] array = {10, 7, 8, 9, 1, 5};
+        QuickSort.Sort(array);
     }
 }
